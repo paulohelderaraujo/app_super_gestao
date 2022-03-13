49877198a35d9131d687111f73beedec95aa8022
Paulo Araujo <paulo.helder@gmail.com>
2022-03-12 23:47:24 -0300

    3º Commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,5 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
--- routes/web.php
+++ routes/web.php
@@ -26,32 +26,23 @@ Route::get('/login', function(){ return 'Login';})->name('site.login');
 //app
 Route::prefix('/app')->group(function() {
 Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
-Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
 Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
 });
 
-Route::get('/rota1', function() {
-    echo 'Rota1';
-})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
 
-Route::get('/rota2', function() {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
+/*
 
+ nome, categoria, assunto mensagem
 
-//Route::redirect('/rota2, rota1');
-
-// nome, categoria, assunto mensagem
-
-/* Rotas afim de teste
-Route::get('/contato/{nome}/{categoria}',
+Rotas afim de teste
+//Route::get('/contato/{nome}/{categoria}'
 function(
     string $nome = 'Paulo Bala',
     int $categoria_id = 1 // 1 - 'Informação'
 ) {
     echo "Estamos aqui carniça: $nome - $categoria_id";
 }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-
-
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+'); */
